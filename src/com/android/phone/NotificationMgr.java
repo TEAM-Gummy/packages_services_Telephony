3d5321e7401be3cdcc8dab0f420b498f269b0101
cphelps76 <cphelps76@gmail.com>
2014-01-18 22:33:45 -0600

    captain break shit strikes again!!! fix notificstion code consistency with 4.4
    Change-Id: I59019fe78cf35eeea6193b3da622ccfced728e87

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -600,7 +600,9 @@ public class NotificationMgr {
              builder.setSmallIcon(android.R.drawable.stat_notify_missed_call)
                 .setTicker(mContext.getString(R.string.notification_missedCallTicker, callName))
                 .setWhen(date)
-                .setContentIntent(PendingIntent.getActivity(mContext, 0, callLogIntent, 0))
+                .setContentTitle(mContext.getText(titleResId))
+                .setContentText(expandedText)
+                .setContentIntent(pendingCallLogIntent)
                 .setAutoCancel(true)
                 .setDeleteIntent(createClearMissedCallsIntent());
         }
