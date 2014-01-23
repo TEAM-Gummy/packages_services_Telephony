PAC-ROM/android_packages_services_Telephony

Breathing Voicemail notifications (1/3)
Change-Id: Ia4531afa1900f65fee5042eddbf83463f0e3e842

src/com/android/phone/NotificationMgr.java
@@ -915,9 +915,14 @@ public class NotificationMgr {
     /* package */ void updateMwi(boolean visible) {
         if (DBG) log("updateMwi(): " + visible);
 
+        int resId;
         if (visible) {
-            int resId = android.R.drawable.stat_notify_voicemail;
-
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1) {
+                resId = R.drawable.stat_notify_voicemail_breath;
+            } else {
+                resId = android.R.drawable.stat_notify_voicemail;
+            }
             // This Notification can get a lot fancier once we have more
             // information about the current voicemail messages.
             // (For example, the current voicemail system can't tell

TEAM-Gummy/packages_services_Telephony

src/com/android/phone/NotificationMgr.java
@@ -895,9 +895,14 @@ public class NotificationMgr {
     /* package */ void updateMwi(boolean visible) {
         if (DBG) log("updateMwi(): " + visible);
 
+        int resId;
         if (visible) {
-            int resId = android.R.drawable.stat_notify_voicemail;
-
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.VOICEMAIL_BREATH, 0) == 1) {
+                resId = R.drawable.stat_notify_voicemail_breath;
+            } else {
+                resId = android.R.drawable.stat_notify_voicemail;
+            }
             // This Notification can get a lot fancier once we have more
             // information about the current voicemail messages.
             // (For example, the current voicemail system can't tell